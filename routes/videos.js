0a502b5ed57827120fa9bf217509135c73c5e2f4
shay20y <120938580+shay20y@users.noreply.github.com>
2023-04-20 09:48:15 +0300

    searchExp added

--- routes/videos.js
+++ routes/videos.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { auth } = require("../middlewares/auth");
 const { VideoModel, validateVideo } = require("../models/videoModel");
 const router = express.Router();
+const countPage = 5
 
 router.get("/", async (req, res) => {
     const perPage = req.query.perPage || 5;
@@ -10,17 +11,21 @@ router.get("/", async (req, res) => {
     const reverse = req.query.reverse == "yes" ? 1 : -1;
     const category = req.query.category;
     const user_id = req.query.user_id;
-
+    const search = req.query.s;
 
     try {
-        let filterFind = {};
+        let filterFind = {}
         if (category) {
             filterFind = { category_code: category }
         }
-        else if(user_id){
-            filterFind = {user_id}
+        else if (user_id) {
+            filterFind = { user_id }
         }
-        const data = await VideoModel
+        else if(search){
+            const searchExp = new RegExp(search , "i") 
+            filterFind = {title : searchExp}
+        }
+        let data = await VideoModel
             .find(filterFind)
             .limit(perPage)
             .skip(page * perPage)
@@ -33,10 +38,23 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.get("/count", async (req, res) => {
+    try {
+        let perPage = req.query.perPage || countPage;
+        // יקבל רק את כמות הרשומות בקולקשן
+        const count = await VideoModel.countDocuments({})
+        res.json({ count, pages: Math.ceil(count / perPage) })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+
 router.get("/single/:id", async (req, res) => {
     try {
         const id = req.params.id
-        const data = await VideoModel.findOne({ _id: id });
+        let data = await VideoModel.findOne({ _id: id });
         res.json(data);
     }
     catch (err) {
@@ -46,12 +64,12 @@ router.get("/single/:id", async (req, res) => {
 })
 
 router.post("/", auth, async (req, res) => {
-    const validBody = validateVideo(req.body);
+    let validBody = validateVideo(req.body);
     if (validBody.error) {
         return res.status(400).json(validBody.error.details);
     }
     try {
-        const video = new VideoModel(req.body);
+        let video = new VideoModel(req.body);
         video.user_id = req.tokenData._id
         await video.save();
         res.json(video)
@@ -63,12 +81,12 @@ router.post("/", auth, async (req, res) => {
 })
 
 router.put("/:id", auth, async (req, res) => {
-    const validBody = validateVideo(req.body);
+    let validBody = validateVideo(req.body);
     if (validBody.error) {
         return res.status(400).json(validBody.error.details);
     }
     try {
-        const id = req.params.id;
+        let id = req.params.id;
         let data;
         if (req.tokenData.role == "admin") {
             data = await VideoModel.updateOne({ _id: id }, req.body);
@@ -86,7 +104,7 @@ router.put("/:id", auth, async (req, res) => {
 
 router.delete("/:id", auth, async (req, res) => {
     try {
-        const id = req.params.id;
+        let id = req.params.id;
         let data;
         if (req.tokenData.role == "admin") {
             data = await VideoModel.deleteOne({ _id: id });
